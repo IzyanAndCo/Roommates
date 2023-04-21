78288c4db865073189be39e2247a02b9ba347858
IzyanAndCo <kalyabin24@gmail.com>
2023-04-22 02:01:21 +0400

    Now the secret key is obtained from the environment

--- instance/config.py
+++ instance/config.py
@@ -6,23 +6,24 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'mysecretkey'
     BLUEPRINTS = ['guests', 'users', 'guest_types', 'authentication']
-    JWT_SECRET_KEY = 'super-secret-key'
     JWT_ACCESS_TOKEN_EXPIRES = 3600  # 1 hour
     JWT_REFRESH_TOKEN_EXPIRES = 604800  # 1 week
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    JWT_SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    JWT_SECRET_KEY = 'super-secret-key'
 
 
 class TestingConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'testing_db.db')
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'testing_db.db')
+    JWT_SECRET_KEY = 'super-secret-key'
